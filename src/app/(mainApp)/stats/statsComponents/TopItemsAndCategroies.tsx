5b1bbfad49bad5645a3d4ae4e51c9e93ecfcbea7
Kennedy <68716247+kennedyng@users.noreply.github.com>
2023-12-12 14:31:45 +0200

    new changes

---
+++ src/app/(mainApp)/stats/statsComponents/TopItemsAndCategroies.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import ItemProgress from "./ItemProgress";
+
+const TopItemsAndCategroies = () => {
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-[65px]">
+      <div>
+        <h1 className="font-medium text-2xl mb-[32px]">Top Items</h1>
+
+        <div className="flex flex-col gap-[49px]">
+          <ItemProgress
+            label="bananane"
+            indicatorColor="#F9A109"
+            percentage={50}
+          />
+          <ItemProgress
+            label="bananane"
+            indicatorColor="#F9A109"
+            percentage={50}
+          />
+          <ItemProgress
+            label="bananane"
+            indicatorColor="#F9A109"
+            percentage={50}
+          />
+        </div>
+      </div>
+      <div>
+        <h1 className="font-medium text-2xl mb-[32px]">Top Categories</h1>
+        <div className="flex flex-col gap-[49px]">
+          <ItemProgress
+            label="bananane"
+            indicatorColor="#56CCF2"
+            percentage={50}
+          />
+          <ItemProgress
+            label="bananane"
+            indicatorColor="#56CCF2"
+            percentage={50}
+          />
+          <ItemProgress
+            label="bananane"
+            indicatorColor="#56CCF2"
+            percentage={50}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopItemsAndCategroies;
---
+++ src/components/ShoppingSidebar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ShoppingSidebar = () => {
+  return <div>ShoppingSidebar</div>;
+};
+
+export default ShoppingSidebar;
