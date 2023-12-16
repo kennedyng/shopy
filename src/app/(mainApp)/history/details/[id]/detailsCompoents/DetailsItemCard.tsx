cb811c80c94dafcc1023bdb5b5c296b45ac6e334
Kennedy <68716247+kennedyng@users.noreply.github.com>
2023-12-16 10:13:30 +0200

    added details page

---
+++ src/app/(mainApp)/history/details/[id]/detailsCompoents/DetailsItemCard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DetailsItemCard = () => {
+  return (
+    <div className="bg-white px-[16px] py-[13px] flex flex-row custom-shadow rounded-xl gap-2 justify-between items-baseline max-w-[183px] cursor-pointer">
+      <p className="text-base font-medium">Cookies Chocolate</p>
+      <span className="text-primary-main text-xs whitespace-nowrap">3 pic</span>
+    </div>
+  );
+};
+
+export default DetailsItemCard;
--- src/app/(mainApp)/history/details/[id]/page.tsx
+++ src/app/(mainApp)/history/details/[id]/page.tsx
@@ -1,11 +1,28 @@
 import React from "react";
-import { MdArrowBack } from "react-icons/md";
+import { MdArrowBack, MdCalendarMonth } from "react-icons/md";
+import DetailsItemCard from "./detailsCompoents/DetailsItemCard";
 const Page = () => {
   return (
     <main>
       <button className="flex flex-row gap-[2px] items-center text-primary-main mb-[35px]">
         <MdArrowBack /> <span className="text-xs font-semibold">back</span>
       </button>
+
+      <h1 className="text-[#34333A] text-[26px] font-bold mb-[20px]">
+        Eero's farewell party
+      </h1>
+
+      <div className="text-[#C1C1C4] flex flex-row gap-[10px] mb-[49px]">
+        <MdCalendarMonth />
+        <span className="text-xs font-medium">Mon 24 200</span>
+      </div>
+
+      <h5 className="text-lg font-medium mb-[18px]">Cookies</h5>
+
+      <div className="flex flex-row gap-[19px]">
+        <DetailsItemCard />
+        <DetailsItemCard />
+      </div>
     </main>
   );
 };
