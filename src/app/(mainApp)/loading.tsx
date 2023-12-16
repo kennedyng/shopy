a5412cd129936b47a95636226e49ad0ec6aedf26
Kennedy <68716247+kennedyng@users.noreply.github.com>
2023-12-16 09:17:57 +0200

    Create loading.tsx

---
+++ src/app/(mainApp)/loading.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Laoding = () => {
+  return (
+    <div className="min-h-screen w-full flex flex-col justify-center items-center">
+      <h1 className="animate-pulse text-lg text-primary-main font-bold ">
+        Shoppingfy{" "}
+      </h1>
+      <h1>Loading...</h1>
+    </div>
+  );
+};
+
+export default Laoding;
