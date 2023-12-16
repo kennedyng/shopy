590e3d887cc11ea7fce1704222ced8dbefa29d2d
Kennedy <68716247+kennedyng@users.noreply.github.com>
2023-12-16 09:45:41 +0200

    added history page

--- src/app/(mainApp)/history/page.tsx
+++ src/app/(mainApp)/history/page.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import HistoryItemCard from "./historyComponents/HistoryItemCard";
 
 const Page = () => {
-  return <div>History</div>;
+  return (
+    <main>
+      <h1 className="text-[#34333A] font-bold text-[24px] mb-[41px]">
+        Shopping history
+      </h1>
+
+      <h6 className="text-xs font-medium mb-[17px]">August 2020</h6>
+      <div className="flex flex-col gap-[28px]">
+        <HistoryItemCard />
+        <HistoryItemCard />
+      </div>
+    </main>
+  );
 };
 
 export default Page;
