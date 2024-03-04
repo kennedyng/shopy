cbe0fe238cda7bfb6616a8288c53bd7a3af30243
kennedy <kennedyngosachanda@gmail.com>
2024-03-04 21:03:28 +0200

    new changes

--- src/components/NewItemSidebar.tsx
+++ src/components/NewItemSidebar.tsx
@@ -1,83 +1,167 @@
 import React, { FC } from "react";
-import Input from "./reusable/Input";
-import TextArea from "./reusable/TextArea";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
 interface Props {
   open: boolean | true | false;
 }
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "./ui/input";
+import { TextArea } from "./ui/text-area";
+const FormSchema = z.object({
+  name: z.string({ required_error: "name is required" }),
+  note: z
+    .string()
+    .min(150, {
+      message: "Username must be at least 8 characters.",
+    })
+    .optional(),
+  image: z
+    .string()
+    .min(150, {
+      message: "Username must be at least 8 characters.",
+    })
+    .optional(),
+
+  category: z
+    .string()
+    .min(150, {
+      message: "Username must be at least 8 characters.",
+    })
+    .optional(),
+});
 const NewItemSidebar: FC<Props> = ({ open }) => {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      name: "",
+      note: "",
+      image: "",
+
+      category: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    alert(JSON.stringify(data));
+  }
   return (
     <aside
       className={`${
         open ? "translate-x-0" : "translate-x-full"
-      } fixed w-[calc(100%-61px)] bg-[#FAFAFE] h-screen top-0 right-0 px-[40px] py-[34px] lg:w-[389px] duration-200`}
+      } fixed w-[calc(100%-61px)] bg-[#FAFAFE] h-screen overflow-y-auto  top-0 right-0 px-[40px] py-[34px] lg:w-[389px] duration-200`}
     >
       <h4 className="mb-[33px] text-lg font-medium">Add a new item</h4>
-
-      <form className="flex flex-col gap-[18px]">
-        <div>
-          <label
-            htmlFor="name"
-            className="mb-[6px] text-sm font-medium text-[#34333A]"
-          >
-            Name
-          </label>
-          <Input
-            id="name"
-            placeholder="Enter a name"
-            className="border-[#BDBDBD] focus:border-primary-main text-sm h-[58px] px-[17px]"
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col gap-[10px]"
+        >
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="mb-[6px] text-sm font-medium text-[#34333A]">
+                  Name
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    className="border-[#BDBDBD] focus:border-primary-main text-sm h-[58px] px-[17px]"
+                    placeholder="kennedyngosachanda@gmail.com"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
           />
-        </div>
 
-        <div>
-          <label
-            htmlFor="note"
-            className="mb-[6px] text-sm font-medium text-[#34333A]"
-          >
-            Note (optional)
-          </label>
-          <TextArea
-            id="note"
-            placeholder="Enter a name"
-            className="border-[#BDBDBD] focus:border-primary-main h-[110px] text-sm  px-[17px]"
+          <FormField
+            control={form.control}
+            name="note"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="mb-[6px] text-sm font-medium text-[#34333A]">
+                  Note (Optional)
+                </FormLabel>
+                <FormControl>
+                  <TextArea
+                    rows={8}
+                    cols={8}
+                    className="border-[#BDBDBD] focus:border-primary-main  text-sm  "
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
           />
-        </div>
 
-        <div>
-          <label
-            htmlFor="image"
-            className="mb-[6px] text-sm font-medium text-[#34333A]"
-          >
-            Image (opotional)
-          </label>
-          <Input
-            id="image"
-            placeholder="Enter a url"
-            className="border-[#BDBDBD] focus:border-primary-main text-sm h-[58px] px-[17px]"
+          <FormField
+            control={form.control}
+            name="image"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="mb-[6px] text-sm font-medium text-[#34333A]">
+                  Image url (Optional)
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    className="border-[#BDBDBD] focus:border-primary-main text-sm h-[58px] px-[17px]"
+                    placeholder="kennedyngosachanda@gmail.com"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
           />
-        </div>
-        <div>
-          <label
-            htmlFor="category"
-            className="mb-[6px] text-sm font-medium text-[#34333A]"
-          >
-            Category
-          </label>
-          <Input
-            id="name"
-            placeholder="Enter a name"
-            className="border-[#BDBDBD] focus:border-primary-main text-sm h-[58px] px-[17px]"
-          />
-        </div>
 
-        <div className="flex justify-center">
-          <div className="flex flex-row gap-[18px]">
-            <button>cancel</button>
-            <button className="bg-primary-main text-white rounded-xl font-bold h-[61px] w-[87px]">
-              Save
-            </button>
+          <FormField
+            control={form.control}
+            name="category"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="mb-[6px] text-sm font-medium text-[#34333A]">
+                  Category
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    className="border-[#BDBDBD] focus:border-primary-main text-sm h-[58px] px-[17px]"
+                    placeholder="kennedyngosachanda@gmail.com"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className="flex justify-center">
+            <div className="flex flex-row gap-[18px]">
+              <button type="button">cancel</button>
+              <button
+                type="submit"
+                className="bg-primary-main text-white rounded-xl font-bold h-[61px] w-[87px]"
+              >
+                Save
+              </button>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </Form>
     </aside>
   );
 };
