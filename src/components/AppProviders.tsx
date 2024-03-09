b0a88c741daa4174b77e97e08eff17759cfbdde6
Kennedy Ngosa <kennedyngosachanda@gmail.com>
2024-03-09 10:25:50 +0200

    global swr configuration

--- src/components/AppProviders.tsx
+++ src/components/AppProviders.tsx
@@ -1,13 +1,23 @@
 "use client";
 
+import apiClient from "@/lib/axios";
 import { SessionProvider } from "next-auth/react";
 import React, { FC, ReactNode } from "react";
+import { SWRConfig } from "swr/_internal";
 
 interface Props {
   children: ReactNode;
 }
+
+const swrOptions = {
+  fetcher: (url: string) => apiClient.get(url).then((res) => res.data), // Example: use axiosInstance for fetching
+};
 const AppProviders: FC<Props> = ({ children }) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <SWRConfig value={swrOptions}>{children}</SWRConfig>
+    </SessionProvider>
+  );
 };
 
 export default AppProviders;
